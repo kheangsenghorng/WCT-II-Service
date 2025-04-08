4a049f14ef95a7f52d95a54e1fcc850c6f753b3e
MacBook Pro <macbookpro@MacBooks-MacBook-Pro-2.local>
2025-04-08 13:02:08 +0700

    fix

---
+++ frontend/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+
+  /* config options here */
+    /* config options here */
+};
+
+export default nextConfig;
