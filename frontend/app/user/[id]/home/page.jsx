7ca9e0514a742f66ee68d6aa3557ac6e0f787393
khornSaokhouch <khornsaokhouch4456@gmail.com>
2025-05-23 13:11:15 +0700

    get total page admin and owner

--- frontend/app/user/[id]/home/page.jsx
+++ frontend/app/user/[id]/home/page.jsx
@@ -1,15 +1,32 @@
 // UserHomePage.js
 "use client";
 import React from "react";
-import { useParams } from "next/navigation";
 import HeroSection from "@/components/HeroSection";
+import WelcomeSection from "@/components/WelcomeSection";
 import ServicesSection from "@/components/ServicesSection";
+import HightQuility from "@/components/HightQuility";
+import ExpertTeamSection from "@/components/ExpertTeamSection";
+import FeedbackSection from "@/components/FeedbackSection";
+import FindUs from "@/components/FindUs";
+import ContactInfo from "@/components/ContactInfo";
+
 
 export default function UserHomePage() {
-    return (
-        <div>
-        <HeroSection />
-        <ServicesSection />
-        </div>
-    );
-    }
+  return (
+    <div>
+      <HeroSection />
+      <WelcomeSection />
+      <ServicesSection />
+      <HightQuility/>
+      {/* <BlogSection/> */}
+      <ExpertTeamSection/>
+      <FeedbackSection />
+      <div className="py-16 px-4 md:px-5 lg:px-24 ">
+      <div className="grid md:grid-cols-2 gap-6 w-full">
+        <FindUs />
+        <ContactInfo />
+      </div>
+      </div>
+    </div>
+  );
+}
