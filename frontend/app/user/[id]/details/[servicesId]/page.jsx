52e98140c20931990b9c93c8a1759899578bcaed
Samnisa <samnisa2104@gmail.com>
2025-06-14 13:16:31 +0700

    detail page

--- frontend/app/user/[id]/details/[servicesId]/page.jsx
+++ frontend/app/user/[id]/details/[servicesId]/page.jsx
@@ -2,10 +2,9 @@
 
 import React from "react";
 import Gallery from "@/components/Gallery"; // Import the Image component
-import Explore from "@/components/Explore";
+// import Explore from "@/components/Explore";
 import BookingForm from "@/components/BookingForm";
 import PhnomPenhTour from "@/components/PhnomPenhTour";
-import AdditionalInfo from "@/components/AdditionalInfo";
 // import Reviews from "@/components/Reviews"; // Make sure you have it or remove it
 import { useParams } from "next/navigation";
 import ServicesForDetails from "@/components/ServicesForDetails"; // Import the ServicesSection component
@@ -17,15 +16,16 @@ function PageTour() {
     <div className="relative min-h-screen bg-gray-50 dark:bg-gray-900 py-12 px-4 sm:px-6 lg:px-8">
       {/* Gallery component */}
       <Gallery servicesId={servicesId} />
+
       {/* Explore component */}
-      <div className="ml-4 mt-4"> 
+      {/* <div className="ml-4 mt-4"> 
       <Explore />
-      </div>
+      </div> */}
+
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
         {/* Main content (left) */}
         <div className="md:col-span-2">
           <PhnomPenhTour />
-          <AdditionalInfo />
           {/* <Reviews /> */}
         </div>
 
